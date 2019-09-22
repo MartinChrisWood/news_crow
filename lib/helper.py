34d082b61e31a3f19d910a7a86027c7fed2b7118
OzwaldCavendish <martin.chris.wood@gmail.com>
2019-09-22 17:46:37 +0100

    Begin pipeline building, add embedding models to library, abstract corpus reader and text cleaner to helper module

---
+++ lib/helper.py
@@ -0,0 +1,50 @@
+"""
+
+Martin Wood - 14/07/2019
+
+For convenience; various functions that the experiment notebooks would be cleaner without
+"""
+
+
+import re
+import os
+import json
+
+import pandas as pd
+
+from datetime import datetime as dt
+
+
+def clean_text(article_text, brutal=False):
+    """ Utility function for cleaning up text for me.  There's probably better ways to prepare data. """
+    article_text = re.sub(r'<b>|</b>|[&#39]', '', article_text)     # Remove annoying tags
+    article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)         # Gets rid of numbers
+    article_text = re.sub(r'\s+', ' ', article_text)                # Replaces all forms of white space with single space
+    if brutal:                                                      # Optional, all non alpha-numeric characters removed
+        article_text = re.sub('r[^0-9A-Za-z ]', "", article_text)
+    return(article_text)
+
+
+def corpus_loader(directory, corpus_type, drop_raw=True):
+    """ For loading my corpus files """
+    
+    # Get a list of all corpus files
+    files = [x for x in os.listdir(directory) if x.endswith(".json") and ("corpus" in x) and (corpus_type in x)]
+    
+    # Implement filters here if I ever feel I need them
+    # Filter by dates if needed
+    #datetimestamps = [dt.strptime(":".join(re.findall(r"[0-9-]{1,}", x)), "%Y-%m-%d:%H%M")  for x in files]
+    
+    # Iterate through and load up every file in sequence
+    compendium = pd.DataFrame()
+    
+    for filename in files:
+        with open(directory + "/" + filename, "r") as f:
+            articles = json.load(f)
+            for article in articles:
+                
+                # Optional, don't bother loading up the original raw response (saves memory)
+                if drop_raw:
+                    article.pop("raw")
+                compendium = compendium.append(article, ignore_index=True)
+    return compendium
