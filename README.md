36cd146916f84d6a4e900f07ac3abefe9ab65ef6
Ozzy Cavendish <martin.chris.wood@gmail.com>
2020-06-26 15:55:36 +0100

    Finished reporting scripts

--- README.md
+++ README.md
@@ -1,7 +1,49 @@
-# Just a collection of (ongoing) experiments
+# Exploring Different Document Clustering Methods
 
-## On clustering and summarizing news articles
+## Testing network-based methods vs word embeddings vs LDA
 
-### Testing different ways of clustering (network vs embedding vs LDA)
 
-#### Not easily reused by others
+### 01_topic_pipeline_network
+
+- Extract all Nouns (names) from all articles using Part-Of-Speech tagging (SpaCy)
+- Apply NPMI to identify nouns that should be conjoined to phrases and treated as a single token
+- Create an adjacency matrix based on co-occurence of nouns
+- Generate a graph from the adjacency matrix, documents become nodes, and non-directional edges exist where > 2 nouns in common are found
+
+For clustering, testing Label Propagation, finding maximally connected cliques, BigClam, and the Louvain community detection algorithm.  A complication with cliques and BigClam is that cliques/BigClam sets are not mutually exclusive - a document can be a member of multiple overlapping cliques.  This is natural for data like this, since many stories will be exploring the interactions of different entities (eg; the "prime minister" and "immigration") and genuinely reflects the corpus' subject matters.  Not sure how to evaluate overlapping clusters yet.
+
+
+### 02_topic_pipeline_HDBSCAN
+
+Pre-processing will include dropping duplicate stories, removing stopwords and lemmatizing the remainder. Additionally, multi-word phrases will be detected and conjoined using Normalised Pointwise Mutual Information (NPMI).  A Continuous-Bag-Of-Words (CBOW) Word2Vec model will be trained on the corpus.  Minimum word count allowed will be set to 1, so that every word found in the training data will be found in the model vocabulary. Vectors are length 100.  Document representations will be created by element-wise averaging of their tokens' vectors.
+
+To create clusters to be treated as topics, the HDBSCAN algorithm will be applied to the document vectors.  Two approaches to applying it will be tried; clustering the vectors directly, and clustering the vectors after applying dimensionality reduction using PCA.
+
+The overall pipeline here has a lot of hyperparameters for both HDBSCAN and Word2Vec.
+
+
+### 03_topic_pipeline_LDA
+
+LDA functions using a Bag-Of-Words (BOW) or Term Frequency - Inverse Document Frequency (TF-IDF) representation.  Pre-processing will include dropping duplicate stories, removing stopwords and lemmatizing the remainder.  Additionally, multi-word phrases will be detected and conjoined using Normalised Pointwise Mutual Information (NPMI).  NPMI functions by calculating how probable it is that two words are found adjacent in a document versus how probable that they are found apart in a document.  The effect is to condense the corpus, joining likely words into their more meaningful phrases and treating those phrases as a single token.  This is more potent than simply fitting an LDA model to both words and all n-grams, as it can act to disambiguate ("domestic\_abuse" and "domestic\_violence" instead of "domestic", "abuse" and "violence") and reduce noise in the model.
+
+LDA requires that the number of topics desired be specified.  An exhaustive search of the number of topics will be performed, with each model being evaluated on perplexity and coherence to find an optimal solution.
+
+
+### 04_clustering_metrics
+
+The clustering/topic modelling methods will be compared on multiple different measures of topic coherence.  The decision may not be clear-cut;  both the network methods and HDBSCAN allow for unassigned outliers which may make that model appear more coherent by expediently dropping any stories that do not fit well.  LDA on the other hand is used to cluster by assigning each document to their most probable topic, and a document's topic probabilities are constrained to sum to 1, so LDA does not designate outliers.  Methods of extractive summarization will later be used to check the quality of clusters, alongside quantitative coherence measures and basic statistics on the topic size/coherence distributions.
+
+
+### 05_text_summarisation
+
+An example of using the TextRank algorithm to summarize text using pre-trained vector models (GLoVe, Doc2Vec) to find and extract the most representative sentences from a short text.
+
+Using pre-trained models because if it works with those such a method could be applied to very small corpuses without issue.
+
+### 06_text_summarisation_bu_analogy
+
+Variant of the above; rather than using text rank, manually define some generic sentence fragments designed to reflect key/summary sentences (eg; "in summary", "to conclude"), and use sentence similarity to these to find the most useful overview/summary/conclusion sentences.
+
+This hasn't worked well yet.
+
+
--- lib/helper.py
+++ lib/helper.py
@@ -7,10 +7,12 @@ import os
 import re
 import json
 import gensim
+import random
 
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 from datetime import datetime as dt
 from gensim.models.coherencemodel import CoherenceModel
@@ -93,19 +95,6 @@ def preprocess_description(description):
     return( [stemmer.stem(token) for token in simple_preprocess(str(description)) if token not in STOPWORDS] )
 
 
-def get_stats(df):
-    """ Helper, for printing basic info on corpus extent """
-    
-    df['doc_size'] = df['clean_text'].apply(lambda x: len(x.split()))
-
-    print(np.mean(df['doc_size']))
-    print(df.shape[0])
-    print(max(df['date']))
-    print(min(df['date']))
-    
-    return 0
-
-
 def get_keyword_stats(df, search_term_path = "D:/Dropbox/news_crow/scrape_settings.json"):
     """Retrive the set of search terms used for Bing, sum stories that contain them """
     with open(search_term_path, "r") as f:
@@ -123,15 +112,15 @@ def get_keyword_stats(df, search_term_path = "D:/Dropbox/news_crow/scrape_settin
     return(term_results)
     
 
-def get_corpus_model_coherence(df, cluster_column="cluster", tokens_column="tokens"):
+def get_corpus_model_coherence(df, cluster_column="cluster"):
     """ 
     Encapsulates entire coherence model-building process for (flat) models
     """
     # Create the vocabulary record
-    bow_dictionary = gensim.corpora.Dictionary(list(df[tokens_column]))
+    bow_dictionary = gensim.corpora.Dictionary(list(df["tokens"]))
     
     # Create a BOW model
-    bow_corpus = [bow_dictionary.doc2bow(doc) for doc in df[tokens_column]]
+    bow_corpus = [bow_dictionary.doc2bow(doc) for doc in df["tokens"]]
     
     # Flattened list of all tokens for all documents for each "topic"
     topics = {}
@@ -139,9 +128,7 @@ def get_corpus_model_coherence(df, cluster_column="cluster", tokens_column="toke
     
     for topic in pd.unique(df['cluster']):
         subset = df[df['cluster'] == topic]
-        
         topics_lengths[topic] = subset.shape[0]
-        
         topics[topic] = flatten(list(subset['tokens']))
     
     # Calculate ALL THE COHERENCE
@@ -164,37 +151,65 @@ def get_corpus_model_coherence(df, cluster_column="cluster", tokens_column="toke
     return(coherence_models, topics_lengths)
 
 
-def report_corpus_model_coherence(df, cluster_column="cluster", tokens_column="tokens"):
+def report_corpus_model_coherence(data_path, cluster_column="cluster", text_column="clean_text", plots=True):
     """
     Creates two key coherence models (C_v, NPMI) and reports coherences,
     plus coherence/topic distribution
     """
-    coherence_models, topics_lengths = get_corpus_model_coherence(df,
-                                                                  cluster_column="cluster",
-                                                                  tokens_column="tokens")
-                                                                  
-    for key in coherence_models.keys():
-        
-        # Extract the model scores and sizes
-        topic_features = pd.DataFrame({"topic_coherence": list(coherence_models[key].get_coherence_per_topic()),
-                                       "topic_sizes": list(topics_lengths.values())})
-    
-    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
-
-    topic_features['topic_sizes'].hist(ax=axs[0])
-    topic_features['topic_coherence'].hist(ax=axs[1])
-    sns.scatterplot(x='topic_sizes', y='topic_coherence', data=topic_features, ax=axs[2])
-
-    return coherence_models
-    
-    
-def load_evaluate_corpus(data_path):
-    """ Helper, process a corpus csv, return its coherence scores """
     df = pd.read_csv(data_path)
+    df["tokens"] = df[text_column].apply(preprocess_description)
     
-    df["tokens"] = df["clean_text"].apply(preprocess_desc)
+    coherence_models, topics_lengths = get_corpus_model_coherence(df, cluster_column=cluster_column)
     
-    print("Number of topics is {}".format(len(pd.unique(df['cluster']))))
+    topics_results = {}
+    if plots:
+        for key in coherence_models.keys():
+        
+            # Extract the model scores and sizes
+            topic_features = pd.DataFrame({"topic_labels": list(topics_lengths.keys()),
+                                           "topic_coherence": list(coherence_models[key].get_coherence_per_topic()),
+                                           "topic_sizes": list(topics_lengths.values())})
+            
+            topics_results[key] = topic_features
+        fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
+        
+        fig.suptitle(data_path)
+        # Diagnostic plots
+        temp = topic_features[topic_features['topic_labels'] != -1]
+        temp['topic_sizes'].hist(ax=axs[0], bins=30)
+        temp['topic_coherence'].hist(ax=axs[1], bins=30)
+        sns.scatterplot(x='topic_sizes', y='topic_coherence', data=temp, ax=axs[2])
     
-    return get_corpus_model_coherence(df)
+    # Key stats
+    for key in coherence_models.keys():
+        print("{measure}: {score}".format(measure=key, score=round(coherence_models[key].get_coherence(), 4)))
+        
+    df['doc_size'] = df['clean_text'].apply(lambda x: len(x.split()))
+    print("Average document word count: {}".format(np.mean(df['doc_size'])))
+    print("Number of documents: {}".format(df.shape[0]))
+    print("Latest record: {}".format(max(df['date'])))
+    print("Earliest record: {}".format(min(df['date'])))
+    print("Number of clusters: {}".format(len(pd.unique(df['cluster']))))
+    print("Median cluster size: {}".format(np.median(topic_features['topic_sizes'])))
+    print("Clustered docs: {}%".format(round(100.0 * sum(df[cluster_column] != -1) / df.shape[0], 1)))
+    
+    # I think for topic-specific coherence, lower == better?
+    top_topics = random.choices(list(topics_results['c_v'].sort_values("topic_coherence", ascending=False)['topic_labels']), k=4)
+    bot_topics = random.choices(list(topics_results['c_v'].sort_values("topic_coherence", ascending=True)['topic_labels']), k=4)
+    pop_topics = random.choices(list(topics_results['c_v'].sort_values("topic_sizes", ascending=False)['topic_labels']), k=4)
+    
+    print("\nBest Performant (C_v)!")
+    for topic in top_topics:
+        print(df[df[cluster_column]==topic][text_column].head(4))
+    
+    print("\nWorst Performant (C_v)!")
+    for topic in bot_topics:
+        print(df[df[cluster_column]==topic][text_column].head(4))
+    
+    print("\nMost Populated!")
+    for topic in pop_topics:
+        print(df[df[cluster_column]==topic][text_column].head(4))
+    
+    return coherence_models, topics_results
+    
     
